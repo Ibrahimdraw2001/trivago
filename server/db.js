d2c3975465af6f2c0371f3576f06874c57cf6bb9
Developer <dev@wallet-app.com>
2026-08-17 08:29:28 +0300

    fix: use h.image instead of empty string when seeding hotels in db.init

--- server/db.js
+++ server/db.js
@@ -187,14 +187,14 @@ async function init() {
   }
 
   const hotelsVersion = await get('SELECT value FROM settings WHERE key = ?', ['hotels_version']);
-  if (!hotelsVersion || hotelsVersion.value !== '3') {
+  if (!hotelsVersion || hotelsVersion.value !== '4') {
     await run('DELETE FROM hotels');
     for (const h of HOTELS) {
       await run('INSERT INTO hotels (name, city, country, image, description) VALUES (?, ?, ?, ?, ?)',
-        [h.name, h.city, h.country, '', h.description]);
+        [h.name, h.city, h.country, h.image || '', h.description]);
     }
     await run("INSERT INTO settings (key, value) VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET value = ?",
-      ['hotels_version', '3', '3']);
+      ['hotels_version', '4', '4']);
   }
 }
 
